ca3de5cfcbdb6d8aeea851dc353c84fcbae68f2e
Igor Tyan <tyanigor89@gmail.com>
2019-03-07 09:38:02 +0200

    initial commit

---
+++ src/main/java/com/forum/integration/ForumIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.forum.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumIntegrationApplication.class, args);
+	}
+
+}
